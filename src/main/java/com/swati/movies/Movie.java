995474f49d333f6d4b701b34d34cdf5513ae1802
M Swati <m.swati4198@gmail.com>
2024-02-27 17:49:53 +0530

    hello from mars

---
+++ src/main/java/com/swati/movies/Movie.java
@@ -0,0 +1,8 @@
+package com.swati.movies;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class Movie {
+    
+}
---
+++ src/test/java/com/swati/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swati.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
